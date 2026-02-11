a3b6de454ccba9e22546ccb395c78d3768fa7a5e
kalishhhh <kaliaishit@gmail.com>
2026-02-11 10:57:26 +0530

    Fix Dockerfile to use HA base image and apk

--- sip-softphone/run.sh
+++ sip-softphone/run.sh
@@ -1,19 +1,3 @@
 #!/usr/bin/with-contenv bash
-
-CONFIG_PATH=/data/options.json
-
-SIP_SERVER=$(jq -r '.sip_server' $CONFIG_PATH)
-EXTENSION=$(jq -r '.extension' $CONFIG_PATH)
-PASSWORD=$(jq -r '.password' $CONFIG_PATH)
-
-cat > /app/pjsua.conf <<EOF
---id sip:$EXTENSION@$SIP_SERVER
---registrar sip:$SIP_SERVER
---realm *
---username $EXTENSION
---password $PASSWORD
---auto-answer 200
---null-audio
-EOF
-
-exec /venv/bin/python /app/server.py
+echo "Addon started successfully"
+sleep infinity
