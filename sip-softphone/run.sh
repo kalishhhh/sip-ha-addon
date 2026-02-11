4bbc796c0837a8caa3927ab1159b81789c5c851f
kalishhhh <kaliaishit@gmail.com>
2026-02-11 09:43:05 +0530

    Initial SIP Addon

---
+++ sip-softphone/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CONFIG_PATH=/data/options.json
+
+SIP_SERVER=$(jq -r '.sip_server' $CONFIG_PATH)
+EXTENSION=$(jq -r '.extension' $CONFIG_PATH)
+PASSWORD=$(jq -r '.password' $CONFIG_PATH)
+
+cat > /app/pjsua.conf <<EOF
+--id sip:$EXTENSION@$SIP_SERVER
+--registrar sip:$SIP_SERVER
+--realm *
+--username $EXTENSION
+--password $PASSWORD
+--auto-answer 200
+--null-audio
+EOF
+
+python3 /app/server.py
